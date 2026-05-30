869a21fce5dc05d13541e09c84c11f4e1e6ff483
たけのこII <yu2.aoki0229.@gmail.com>
2026-05-30 23:55:47 +0900

    a

--- src/main/kotlin/io/github/takenoko4096/noctiluca/event/ServerSideEventListener.kt
+++
@@ -1,5 +0,0 @@
-package io.github.takenoko4096.noctiluca.event
-
-object ServerSideEventListener {
-
-}
--- src/main/kotlin/io/github/takenoko4096/noctiluca/execute/Function.kt
+++ src/main/kotlin/io/github/takenoko4096/noctiluca/execute/Function.kt
@@ -1,14 +1,1 @@
 package io.github.takenoko4096.noctiluca.execute
-
-import io.github.takenoko4096.noctiluca.execute.arguments.selector.EntitySelector
-import io.github.takenoko4096.noctiluca.execute.arguments.selector.SelectorArgument
-import net.minecraft.world.entity.EntityType
-
-fun execute() {
-    EntitySelector.`@s` {
-        x(0.0)
-        y(0.1)
-        z(-2.0)
-        type(EntityType.ALLAY)
-    }
-}
