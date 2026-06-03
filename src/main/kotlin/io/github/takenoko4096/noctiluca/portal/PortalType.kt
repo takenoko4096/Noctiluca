f224e5cf0da5122daabe659ca81ff328989b5cc1
たけのこII <yu2.aoki0229.@gmail.com>
2026-06-03 16:37:15 +0900

    a

--- src/main/kotlin/io/github/takenoko4096/noctiluca/portal/PortalType.kt
+++ src/main/kotlin/io/github/takenoko4096/noctiluca/portal/PortalType.kt
@@ -23,11 +23,11 @@ class PortalType private constructor(val id: Int, val frameBlock: Block, val tin
         private var maximum = 0
 
         fun register(frameBlock: Block, tintColor: ArgbColor, ignitionSource: Item, maxWidth: Int = 21, maxHeight: Int = 21): PortalType {
-            val id = maximum++
-            val type = PortalType(id, frameBlock, tintColor, ignitionSource, maxWidth, maxHeight)
-            if (type in types.values) {
+            if (types.values.any { it.frameBlock == frameBlock }) {
                 throw IllegalArgumentException("ポータルフレームに使用できないブロックです: 既に使用されています")
             }
+            val id = maximum++
+            val type = PortalType(id, frameBlock, tintColor, ignitionSource, maxWidth, maxHeight)
             types[id] = type
             return type
         }
